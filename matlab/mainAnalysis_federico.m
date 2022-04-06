82c68a77716585db84b3b548fc90b230f2d92eef
Federico Tessari <chopp@MSI.mit.edu>
2022-04-06 17:17:31 -0400

    modifications on the code with James

--- matlab/mainAnalysis_federico.m
+++ matlab/mainAnalysis_federico.m
@@ -16,39 +16,39 @@ classdef mainAnalysis_federico < handle
             dbstop if error
             addpath('func/');
 
-            %Spring Test
-            load('data/ss3334_3344_alter.mat');
-            dexForce_spring = 1:3; % [15N, 20N, 25N]
-            data_spring = data;
-            for force = dexForce_spring
-                for stiff = 1:3
-                    for trial = 1:15
-                        for pret = 1:3
-                            data_spring{1,force,stiff+1,trial,pret} = data{1,force,stiff,trial,pret};
-                        end
-                    end
-                end
-            end
-            clear data
-            load('data/ss3345.mat');
-            for force = dexForce_spring
-                for trial = 1:15
-                    for pret = 1:3
-                    data_spring{1,force,1,trial,pret} = data{1,1,1,trial,pret};
-                    end
-                end
-            end
-            clear data
-            dexStiff_spring = 1:4; 
-            depMeasures_spring = crossConditionAnalysis(data_spring,1,dexForce_spring,dexStiff_spring,'spring');
+%             %Spring Test
+%             load('data/ss3334_3344_alter.mat');
+%             dexForce_spring = 1:3; % [15N, 20N, 25N]
+%             data_spring = data;
+%             for force = dexForce_spring
+%                 for stiff = 1:3
+%                     for trial = 1:15
+%                         for pret = 1:3
+%                             data_spring{1,force,stiff+1,trial,pret} = data{1,force,stiff,trial,pret};
+%                         end
+%                     end
+%                 end
+%             end
+%             clear data
+%             load('data/ss3345.mat');
+%             for force = dexForce_spring
+%                 for trial = 1:15
+%                     for pret = 1:3
+%                     data_spring{1,force,1,trial,pret} = data{1,1,1,trial,pret};
+%                     end
+%                 end
+%             end
+%             clear data
+%             dexStiff_spring = 1:4; 
+%             depMeasures_spring = crossConditionAnalysis(data_spring,1,dexForce_spring,dexStiff_spring,'spring');
  
             %Combined Many subject Tests (12/10/2021)
             load('data/prelimData_6subj_fine.mat');
             data_human = reshape(data(:,1,:,:,:,:),6,3,3,15,3); % Elliminate direction
             clear data
-            dexSubject = 1:6;
-            dexForce = 1:3; % [15N, 20N, 25N]
-            dexDistance = 1:3; % [2.5cm, 5cm, 7.5cm]
+            dexSubject = 1;%:6;
+            dexForce = 1;%:3; % [15N, 20N, 25N]
+            dexDistance = 1;%:3; % [2.5cm, 5cm, 7.5cm]
             depMeasures_human = crossConditionAnalysis(data_human, dexSubject, dexForce, dexDistance,'human');
             
            
@@ -60,7 +60,7 @@ classdef mainAnalysis_federico < handle
 
             %% Plot
             % Make plots Spring
-            depMeasures_spring.get_mainPlot('spring',1,pathh);
+%             depMeasures_spring.get_mainPlot('spring',1,pathh);
              
             % Make plots subjects
             for i = 1
